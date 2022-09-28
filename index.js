334f4a19b2f39fcef206955bd467ee6c71909f77
athamov <abdulloxathamoff@gmail.com>
2022-09-28 19:43:51 +0500

    netlify cookie

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin:'http://localhost:3000',
+  origin:process.env.CLIENT_URL,
   // AccessControlAllowOrigin:'*'
 }));
 app.use('/api',router); 
