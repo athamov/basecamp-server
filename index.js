8d85a0de04b77ac75c6a7f8f10077ef16695ad02
athamov <abdulloxathamoff@gmail.com>
2023-01-05 16:12:49 +0500

    Access-Control-Allow-Origin turn off

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(cors({
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+  // res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_URL);
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
